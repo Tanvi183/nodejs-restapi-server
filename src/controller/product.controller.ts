450925407773dc0db0e09a611f82baf5520f116d
Mohammed Sana Ullah <61879595+Tanvi183@users.noreply.github.com>
2026-05-17 16:03:11 +0600

    Get product by id

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,5 +1,6 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
 import { readProducts } from "../service/product.service";
+import type { IProduct } from "../types/product.type";
 
 
 export const productController = (
@@ -10,8 +11,12 @@ export const productController = (
     const url = req.url;
     const method = req.method;
 
-    if (url === "/products" && method === "GET") {
+    const urlParts = url?.split("/");
+    // console.log(urlParts); // ['', 'products', '123']
+    const id = urlParts && urlParts[1] === 'products' ? Number(urlParts[2]) : null;
+    // console.log(id); // 123
 
+    if (url === "/products" && method === "GET") {      // Get all products
         const products = readProducts();
 
         res.writeHead(200, { "Content-Type": "application/json" });
@@ -20,6 +25,19 @@ export const productController = (
             JSON.stringify({
                 message: "Products retrieved successfully",
                 data: products
+            })
+        );
+    } else if (method === "GET" && id !== null) {   // Get product by id
+        const products = readProducts();
+
+        const product = products.find((p : IProduct) => p.id === id);
+
+        // console.log(product);
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(
+            JSON.stringify({
+                message: "Product retrieved successfully",
+                data: product
             })
         );
     }
